7ed377b35415458111913e692e9b273b4d808f88
manuel <manuel@manuel.manuel>
2025-02-16 16:59:01 -0300

    uninstalled cookies

--- src/services.ts
+++ src/services.ts
@@ -1,12 +1,6 @@
 import axios from "axios";
-import Cookies from 'js-cookie'
-const cookies = Cookies.get()
-
-const cookiesString = Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ')
-
 const headers = {
   'Content-Type': 'application/json',
-  'Cookie': cookiesString
 }
 const axiosInstance = axios.create({
   baseURL: 'https://yt-downloader-back.vercel.app',
