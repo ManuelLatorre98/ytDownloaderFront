e1270cb331ef5542f44feeaf9a35c75fe284a807
manuel <manuel@manuel.manuel>
2025-02-16 13:18:02 -0300

    added cookies

--- src/services.ts
+++ src/services.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
+import Cookies from 'js-cookie'
+const cookies = Cookies.get()
 
+const cookiesString = Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ')
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Cookie': cookiesString
+}
 const axiosInstance = axios.create({
   baseURL: 'https://yt-downloader-back.vercel.app',
   timeout: 60000,
-  headers: {'Content-Type': 'application/json'},
+  headers: headers,
   withCredentials: true
 })
 
