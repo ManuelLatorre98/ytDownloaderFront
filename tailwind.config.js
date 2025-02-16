3a4de1d0ffd3f68009c426e1dfa9894e8bc3958c
manuel <manuel@manuel.manuel>
2025-02-16 12:32:07 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
-
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
